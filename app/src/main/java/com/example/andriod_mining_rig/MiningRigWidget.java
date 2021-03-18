f3c144e4daa871e728d3511b3bc0b5ea3f7773e6
braydenwerner <66924360+braydenwerner@users.noreply.github.com>
2021-03-17 21:59:03 -0700

    update

---
+++ app/src/main/java/com/example/andriod_mining_rig/MiningRigWidget.java
@@ -0,0 +1,46 @@
+package com.example.andriod_mining_rig;
+
+import android.app.AlarmManager;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class MiningRigWidget extends AppWidgetProvider {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        widgetText = "This is a string";
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.mining_rig_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+       // views.setTextViewText(R.id.active_workers, "69");
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+        System.out.println ("creating widget");
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+        System.out.println ("removing widget");
+    }
+}
+
